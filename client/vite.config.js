dabb41aad94536d952646fbbea863045fa423cb4
Raj Ranjan <rajputsunnysingh54@gmail.com>
2026-02-05 04:52:22 +0530

    up

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPO_NAME/', // TODO: Update this to match your GitHub repository name (e.g., '/my-portfolio/')
+  base: '/My-Portfolio/', // TODO: Update this to match your GitHub repository name (e.g., '/my-portfolio/')
 })
